92da215babe856da3e8fd5f0d9b73586e9014ffb
Tony Fong <tonyfong@outlook.com>
2019-08-07 13:26:32 +0800

    Updates

---
+++ precache-manifest.38d62205c2f67597a8f47d3f156bd863.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a1b38d787fc1b17f787057d1db0a22",
+    "url": "/react-monstors/index.html"
+  },
+  {
+    "revision": "c276457803c64716c993",
+    "url": "/react-monstors/static/css/main.7d244f85.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-monstors/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c276457803c64716c993",
+    "url": "/react-monstors/static/js/main.6317a77d.chunk.js"
+  },
+  {
+    "revision": "d2eca6179b36e4611dc1",
+    "url": "/react-monstors/static/js/runtime~main.bbdc7d49.js"
+  }
+]);
